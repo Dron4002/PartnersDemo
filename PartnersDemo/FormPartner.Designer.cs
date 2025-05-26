bb9392b12ec0dd01bd337dfb371a8cb6dadda395
bylen <bylen@DESKTOP-90NUGUN>
2025-05-26 12:56:15 +0300

    Добавьте файлы проекта.

---
+++ PartnersDemo/FormPartner.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace PartnersDemo
+{
+    partial class FormPartner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            comboBox1 = new ComboBox();
+            maskedTextBox1 = new MaskedTextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label1.ForeColor = Color.FromArgb(103, 186, 128);
+            label1.Location = new Point(164, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(170, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Добавление партнера";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(24, 70);
+            label2.Name = "label2";
+            label2.Size = new Size(116, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Наименование";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(33, 108);
+            label3.Name = "label3";
+            label3.Size = new Size(35, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Тип";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(33, 153);
+            label4.Name = "label4";
+            label4.Size = new Size(64, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Рейтинг";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(33, 191);
+            label5.Name = "label5";
+            label5.Size = new Size(51, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Адрес";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(33, 237);
+            label6.Name = "label6";
+            label6.Size = new Size(119, 20);
+            label6.TabIndex = 5;
+            label6.Text = "ФИО директора";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(33, 284);
+            label7.Name = "label7";
+            label7.Size = new Size(69, 20);
+            label7.TabIndex = 6;
+            label7.Text = "Телефон";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(33, 333);
+            label8.Name = "label8";
+            label8.Size = new Size(143, 20);
+            label8.TabIndex = 7;
+            label8.Text = "Электронная почта";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(185, 67);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(501, 27);
+            textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(185, 150);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(501, 27);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(185, 191);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(501, 27);
+            textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(185, 237);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(501, 27);
+            textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(185, 333);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(501, 27);
+            textBox5.TabIndex = 12;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(184, 108);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(502, 28);
+            comboBox1.TabIndex = 13;
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(184, 284);
+            maskedTextBox1.Mask = "+7 000 000 00 00";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(502, 27);
+            maskedTextBox1.TabIndex = 14;
+            maskedTextBox1.MaskInputRejected += maskedTextBox1_MaskInputRejected;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(173, 385);
+            button1.Name = "button1";
+            button1.Size = new Size(183, 29);
+            button1.TabIndex = 15;
+            button1.Text = "Добавить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            button1.MouseEnter += button1_MouseEnter;
+            button1.MouseLeave += button1_MouseLeave;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(8, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 16;
+            button2.Text = "Назад";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // FormPartner
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(maskedTextBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FormPartner";
+            Text = "FormPartner";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private ComboBox comboBox1;
+        private MaskedTextBox maskedTextBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
